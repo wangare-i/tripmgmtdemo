be80bb174bec95eaffce08d924b1b954c790b295
wangare-i <wangareirungu@gmail.com>
2025-02-17 12:07:43 +0000

    added

---
+++ tripmgmt/src/test/java/com/example/travel/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.example.travel;
+
+import com.example.travel.TripmgmtApp;
+import com.example.travel.config.AsyncSyncConfiguration;
+import com.example.travel.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { TripmgmtApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
